c5aee06f8911a66c1fc3bbbcf20dd47d97a80ad5
D1mao <grinlight53721@mail.ru>
2023-09-30 10:09:57 +0300

    changed  cycles1 for using method

--- cycles1.java
+++ cycles1.java
@@ -7,22 +7,23 @@ public class cycles1 {
         double x = scan.nextDouble();
         double x_end = scan.nextDouble();
         double step = scan.nextDouble();
-        double y;
         System.out.println("Значение аргумента    Значение функции");
         while (x < x_end) {
-            if (x >= -9 && x < -6){
-                y = -(Math.pow(-(x*x)-12*x-27, 0.5));
-            } else if (x >= -6 && x <= -3){
-                y = x + 3;
-            } else if (x > -3 && x <= 0){
-                y = Math.pow(-(x*x)+9, 0.5);
-            } else if (x > 0 && x <= 3){
-                y = -x + 3;
-            } else {
-                y = 0.5*(x - 3);
-            }
-            System.out.println(x + "                     " + y);
+            System.out.println(x + "                     " + formulas(x));
             x += step;
+        }
+    }
+    public static double formulas(double x){
+        if (x >= -9 && x < -6){
+            return -(Math.pow(-(x*x)-12*x-27, 0.5));
+        } else if (x >= -6 && x <= -3){
+            return x + 3;
+        } else if (x > -3 && x <= 0){
+            return Math.pow(-(x*x)+9, 0.5);
+        } else if (x > 0 && x <= 3){
+            return  -x + 3;
+        } else {
+            return  0.5*(x - 3);
         }
     }
 }
